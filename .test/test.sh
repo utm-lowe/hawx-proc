f195dff92febab28e147e34678618d762b7e8638
Robert Lowe <rlowe@semo.edu>
2026-03-15 19:33:05 +0000

    updated test

--- .test/test.sh
+++ .test/test.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-expected=4
+expected=3
 
 .test/run-hawx > .test/hawx.out
-#passed=$(grep PASSED .test/hawx.out | wc -l)
+passed=$(grep PASSED .test/hawx.out | wc -l)
 
 cat .test/hawx.out
 
-#echo
-#echo
-#echo "Passed $passed out of $expected tests."
+echo
+echo
+echo "Passed $passed out of $expected tests."
 
-#if [ $passed -ne $expected ]; then exit 1; fi
+killall qemu-system-riscv64
+
+if [ $passed -ne $expected ]; then exit 1; fi
